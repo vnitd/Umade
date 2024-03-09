ac11f91513dc49123ed123c6fabdd03d52ed1f0a
myduyen3103 <vtmyduyen3103@gmail.com>
2024-03-09 22:35:32 +0700

    import font from MUI

--- umade-client/src/pages/UserSetting/ChangePassword.jsx
+++ umade-client/src/pages/UserSetting/ChangePassword.jsx
@@ -13,7 +13,10 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import axios from 'axios'
-
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 //Footer copyright
 function Copyright(props) {
 	return (
